6be27b4d287cb133e7f8c7d6892e5b0cbffdbcd3
Yasuhiro Ikeda <wipple625@gmail.com>
2011-12-25 15:26:47 +0000

    Print video info with lavf/ffms demuxer

--- input/ffms.c
+++ input/ffms.c
@@ -32,6 +32,7 @@
 #undef DECLARE_ALIGNED
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
+#include <libavutil/pixdesc.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -167,9 +168,9 @@ static int open_file( char *psz_filename, hnd_t *p_handle, video_info_t *info, c
 
     /* ffms timestamps are in milliseconds. ffms also uses int64_ts for timebase,
      * so we need to reduce large timebases to prevent overflow */
+    const FFMS_TrackTimeBase *timebase = FFMS_GetTimeBase( h->track );
     if( h->vfr_input )
     {
-        const FFMS_TrackTimeBase *timebase = FFMS_GetTimeBase( h->track );
         int64_t timebase_num = timebase->Num;
         int64_t timebase_den = timebase->Den * 1000;
         h->reduce_pts = 0;
@@ -183,6 +184,29 @@ static int open_file( char *psz_filename, hnd_t *p_handle, video_info_t *info, c
         info->timebase_num = timebase_num;
         info->timebase_den = timebase_den;
     }
+
+    /* show video info */
+    FFMS_Indexer *idxer    = FFMS_CreateIndexer( src_filename, &e );
+    const char *format     = FFMS_GetFormatNameI( idxer );
+    const char *codec      = FFMS_GetCodecNameI( idxer, trackno );
+    double duration        = videop->NumFrames * videop->FPSDenominator / videop->FPSNumerator;
+    const AVPixFmtDescriptor *pix_desc = av_pix_fmt_desc_get(frame->EncodedPixelFormat);
+    x264_cli_log( "ffms", X264_LOG_INFO,
+                  "\n Format    : %s"
+                  "\n Codec     : %s"
+                  "\n PixFmt    : %s"
+                  "\n Framerate : %d/%d"
+                  "\n Timebase  : %"PRIu64"/%"PRIu64
+                  "\n Duration  : %d:%02d:%02d\n",
+                  format,
+                  codec,
+                  pix_desc->name,
+                  videop->FPSNumerator, videop->FPSDenominator,
+                  (uint64_t)timebase->Num, (uint64_t)timebase->Den * 1000,
+                  (int)duration / 60 / 60, (int)duration / 60 % 60, (int)duration - (int)duration / 60 * 60 );
+    if( !strcmp( codec,"rawvideo" ) )
+        x264_cli_log( "ffms", X264_LOG_WARNING, "recommend using --demuxer lavf with rawvideo" );
+    FFMS_CancelIndexing( idxer );
 
     *p_handle = h;
     return 0;
