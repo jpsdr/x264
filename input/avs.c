0dfcad252fbfb76e574882258db24d106fa578b8
Lucien <astrataro@gmail.com>
2011-12-25 15:41:08 +0000

    Fix AviSynth color space converting matrix

--- input/avs.c
+++ input/avs.c
@@ -396,7 +396,8 @@ static int open_file( char *psz_filename, hnd_t *p_handle, video_info_t *info, c
         {
             // if converting from yuv, then we specify the matrix for the input, otherwise use the output's.
             int use_pc_matrix = avs_is_yuv( vi ) ? opt->input_range == RANGE_PC : opt->output_range == RANGE_PC;
-            snprintf( matrix, sizeof(matrix), "%s601", use_pc_matrix ? "PC." : "Rec" ); /* FIXME: use correct coefficients */
+            strcpy( matrix, use_pc_matrix ? "PC." : "Rec" );
+            strcat( matrix, ( vi->width > 1024 || vi->height > 576 ) ? "709" : "601" );
             arg_count++;
             // notification that the input range has changed to the desired one
             opt->input_range = opt->output_range;
