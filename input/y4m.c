0422528863d56ae516bb4414c98bfd697b8eb857
Lucien <astrataro@gmail.com>
2011-12-25 15:28:38 +0000

    Skip bit depth filter when possible

--- input/y4m.c
+++ input/y4m.c
@@ -195,7 +195,14 @@ static int open_file( char *psz_filename, hnd_t *p_handle, video_info_t *info, c
     info->csp         = colorspace;
 
     if( h->bit_depth > 8 )
+    {
         info->csp |= X264_CSP_HIGH_DEPTH;
+        if( h->bit_depth == BIT_DEPTH )
+        {
+            /* HACK: totally skips depth filter to prevent dither error */
+            info->csp |= X264_CSP_SKIP_DEPTH_FILTER;
+        }
+    }
 
     const x264_cli_csp_t *csp = x264_cli_get_csp( info->csp );
 
@@ -275,7 +282,7 @@ static int read_frame_internal( cli_pic_t *pic, y4m_hnd_t *h, int bit_depth_uc )
         else if( fread( pic->img.plane[i], pixel_depth, h->plane_size[i], h->fh ) != h->plane_size[i] )
             return -1;
 
-        if( bit_depth_uc )
+        if( bit_depth_uc && h->bit_depth != BIT_DEPTH )
         {
             /* upconvert non 16bit high depth planes to 16bit using the same
              * algorithm as used in the depth filter. */
