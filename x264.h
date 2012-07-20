3b8f7c75fd3796668743ee35cb4c742f4f208d56
Lucien <astrataro@gmail.com>
2012-07-20 17:16:16 +0100

    Add an option to use r2204 style progress indicator

--- x264.h
+++ x264.h
@@ -363,6 +363,7 @@ typedef struct x264_param_t
     void        *p_log_private;
     int         i_log_level;
     int         b_full_recon;   /* fully reconstruct frames, even when not necessary for encoding.  Implied by psz_dump_yuv */
+    int         b_stylish;
     char        *psz_dump_yuv;  /* filename (in UTF-8) for reconstructed frames */
 
     /* Encoder analyser parameters */
