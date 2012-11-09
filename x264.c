1b814fe195e0104db5caf6d93defa4373cc47d40
Lucien <astrataro@gmail.com>
2012-11-09 20:39:11 +0000

    Support "vpy" file extension

--- x264.c
+++ x264.c
@@ -1470,7 +1470,8 @@ static int select_input( const char *demuxer, char *used_demuxer, char *filename
     }
     const char *module = b_auto ? ext : demuxer;
 
-    if( !strcasecmp( module, "avs" ) || !strcasecmp( ext, "d2v" ) || !strcasecmp( ext, "dga" ) || !strcasecmp( ext, "dgi" ) )
+    if( !strcasecmp( module, "avs" ) || !strcasecmp( ext, "d2v" ) || !strcasecmp( ext, "dga" )
+     || !strcasecmp( ext, "dgi" ) || !strcasecmp( ext, "vpy" ) )
     {
 #if HAVE_AVS
         cli_input = avs_input;
