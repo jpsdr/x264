silverfilain/x264_L-SMASH

Separate I/P/B qpmin/qpmax parameters

common/common.c
@@ -108,8 +108,14 @@ void x264_param_default( x264_param_t *param )
     param->rc.f_vbv_buffer_init = 0.9;
     param->rc.i_qp_constant = 23 + QP_BD_OFFSET;
     param->rc.f_rf_constant = 23;
-    param->rc.i_qp_min = 0;
-    param->rc.i_qp_max = QP_MAX;
+    param->rc.i_qp_min_min           =
+    param->rc.i_qp_min[SLICE_TYPE_I] =
+    param->rc.i_qp_min[SLICE_TYPE_P] =
+    param->rc.i_qp_min[SLICE_TYPE_B] = 0;
+    param->rc.i_qp_max_max           =
+    param->rc.i_qp_max[SLICE_TYPE_I] =
+    param->rc.i_qp_max[SLICE_TYPE_P] =
+    param->rc.i_qp_max[SLICE_TYPE_B] = QP_MAX;
     param->rc.i_qp_step = 4;
     param->rc.f_ip_factor = 1.4;
     param->rc.f_pb_factor = 1.3;
@@ -1227,9 +1233,21 @@ int x264_param_parse( x264_param_t *p, const char *name, const char *value )
     OPT("rc-lookahead")
         p->rc.i_lookahead = atoi(value);
     OPT2("qpmin", "qp-min")
-        p->rc.i_qp_min = atoi(value);
+    {
+        if( 3 == sscanf( value, "%d:%d:%d", &p->rc.i_qp_min[SLICE_TYPE_I], &p->rc.i_qp_min[SLICE_TYPE_P], &p->rc.i_qp_min[SLICE_TYPE_B] ) ||
+            3 == sscanf( value, "%d,%d,%d", &p->rc.i_qp_min[SLICE_TYPE_I], &p->rc.i_qp_min[SLICE_TYPE_P], &p->rc.i_qp_min[SLICE_TYPE_B] ) )
+            p->rc.i_qp_min_min = X264_MIN3( p->rc.i_qp_min[SLICE_TYPE_I], p->rc.i_qp_min[SLICE_TYPE_P], p->rc.i_qp_min[SLICE_TYPE_B] );
+        else if( sscanf( value, "%d", &p->rc.i_qp_min_min ) )
+            p->rc.i_qp_min[SLICE_TYPE_I] = p->rc.i_qp_min[SLICE_TYPE_P] = p->rc.i_qp_min[SLICE_TYPE_B] = p->rc.i_qp_min_min;
+    }
     OPT2("qpmax", "qp-max")
-        p->rc.i_qp_max = atoi(value);
+    {
+        if( 3 == sscanf( value, "%d:%d:%d", &p->rc.i_qp_max[SLICE_TYPE_I], &p->rc.i_qp_max[SLICE_TYPE_P], &p->rc.i_qp_max[SLICE_TYPE_B] ) ||
+            3 == sscanf( value, "%d,%d,%d", &p->rc.i_qp_max[SLICE_TYPE_I], &p->rc.i_qp_max[SLICE_TYPE_P], &p->rc.i_qp_max[SLICE_TYPE_B] ) )
+            p->rc.i_qp_max_max = X264_MAX3( p->rc.i_qp_max[SLICE_TYPE_I], p->rc.i_qp_max[SLICE_TYPE_P], p->rc.i_qp_max[SLICE_TYPE_B] );
+        else if( sscanf( value, "%d", &p->rc.i_qp_max_max ) )
+            p->rc.i_qp_max[SLICE_TYPE_I] = p->rc.i_qp_max[SLICE_TYPE_P] = p->rc.i_qp_max[SLICE_TYPE_B] = p->rc.i_qp_max_max;
+    }
     OPT2("qpstep", "qp-step")
         p->rc.i_qp_step = atoi(value);
     OPT("ratetol")
@@ -1848,8 +1866,11 @@ char *x264_param2string( x264_param_t *p, int b_res )
         else
             s += sprintf( s, " bitrate=%d ratetol=%.1f",
                           p->rc.i_bitrate, p->rc.f_rate_tolerance );
-        s += sprintf( s, " qcomp=%.2f qpmin=%d qpmax=%d qpstep=%d",
-                      p->rc.f_qcompress, p->rc.i_qp_min, p->rc.i_qp_max, p->rc.i_qp_step );
+        s += sprintf( s, " qcomp=%.2f qpmin=%d:%d:%d qpmax=%d:%d:%d qpstep=%d",
+                      p->rc.f_qcompress,
+                      p->rc.i_qp_min[SLICE_TYPE_I], p->rc.i_qp_min[SLICE_TYPE_P], p->rc.i_qp_min[SLICE_TYPE_B],
+                      p->rc.i_qp_max[SLICE_TYPE_I], p->rc.i_qp_max[SLICE_TYPE_P], p->rc.i_qp_max[SLICE_TYPE_B],
+                      p->rc.i_qp_step );
         if( p->rc.b_stat_read )
             s += sprintf( s, " cplxblur=%.1f qblur=%.1f",
                           p->rc.f_complexity_blur, p->rc.f_qblur );

encoder/analyse.c
@@ -2958,7 +2958,7 @@ static inline void x264_mb_analyse_qp_rd( x264_t *h, x264_mb_analysis_t *a )
         {
             if( !origcbp )
             {
-                h->mb.i_qp = X264_MAX( h->mb.i_qp - threshold - 1, SPEC_QP( h->param.rc.i_qp_min ) );
+                h->mb.i_qp = X264_MAX( h->mb.i_qp - threshold - 1, SPEC_QP( h->param.rc.i_qp_min[h->sh.i_type] ) );
                 h->mb.i_chroma_qp = h->chroma_qp_table[h->mb.i_qp];
                 already_checked_cost = x264_rd_cost_mb( h, a->i_lambda2 );
                 if( !h->mb.cbp[h->mb.i_mb_xy] )
@@ -2975,7 +2975,7 @@ static inline void x264_mb_analyse_qp_rd( x264_t *h, x264_mb_analysis_t *a )
         }
 
         h->mb.i_qp += direction;
-        while( h->mb.i_qp >= h->param.rc.i_qp_min && h->mb.i_qp <= SPEC_QP( h->param.rc.i_qp_max ) )
+        while( h->mb.i_qp >= h->param.rc.i_qp_min[h->sh.i_type] && h->mb.i_qp <= SPEC_QP( h->param.rc.i_qp_max[h->sh.i_type] ) )
         {
             if( h->mb.i_last_qp == h->mb.i_qp )
                 last_qp_tried = 1;
@@ -3076,7 +3076,7 @@ intra_analysis:
         {
             if( !h->param.analyse.b_psy )
             {
-                x264_mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min ) );
+                x264_mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min[h->sh.i_type] ) );
                 goto intra_analysis;
             }
         }
@@ -3355,7 +3355,7 @@ skip_analysis:
                     h->mc.copy[PIXEL_8x8]  ( h->mb.pic.p_fenc[1], FENC_STRIDE, h->mb.pic.p_fdec[1], FDEC_STRIDE, height );
                     h->mc.copy[PIXEL_8x8]  ( h->mb.pic.p_fenc[2], FENC_STRIDE, h->mb.pic.p_fdec[2], FDEC_STRIDE, height );
                 }
-                x264_mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min ) );
+                x264_mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min[h->sh.i_type] ) );
                 goto intra_analysis;
             }
 

tools/checkasm.c
@@ -1801,8 +1801,14 @@ static int check_quant( int cpu_ref, int cpu_new )
             h->param.i_cqm_preset = h->pps->i_cqm_preset = X264_CQM_CUSTOM;
         }
 
-        h->param.rc.i_qp_min = 0;
-        h->param.rc.i_qp_max = QP_MAX;
+        h->param.rc.i_qp_min_min           =
+        h->param.rc.i_qp_min[SLICE_TYPE_I] =
+        h->param.rc.i_qp_min[SLICE_TYPE_P] =
+        h->param.rc.i_qp_min[SLICE_TYPE_B] = 0;
+        h->param.rc.i_qp_max_max           =
+        h->param.rc.i_qp_max[SLICE_TYPE_I] =
+        h->param.rc.i_qp_max[SLICE_TYPE_P] =
+        h->param.rc.i_qp_max[SLICE_TYPE_B] = QP_MAX;
         x264_cqm_init( h );
         x264_quant_init( h, 0, &qf_c );
         x264_quant_init( h, cpu_ref, &qf_ref );
@@ -1833,7 +1839,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( #name ); \
             used_asms[0] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int j = 0; j < 2; j++ ) \
                 { \
@@ -1859,7 +1865,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( #qname ); \
             used_asms[0] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int j = 0; j < maxj; j++ ) \
                 { \
@@ -1892,7 +1898,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( "%s_%s", #dqname, i_cqm?"cqm":"flat" ); \
             used_asms[1] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 INIT_QUANT##w(1, w*w) \
                 qf_c.qname( dct1, h->quant##w##_mf[block][qp], h->quant##w##_bias[block][qp] ); \
@@ -1920,7 +1926,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( "%s_%s", #dqname, i_cqm?"cqm":"flat" ); \
             used_asms[1] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int i = 0; i < 16; i++ ) \
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16; \
@@ -1944,7 +1950,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         {
             set_func_name( "idct_dequant_2x4_dc_%s", i_cqm?"cqm":"flat" );
             used_asms[1] = 1;
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- )
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             {
                 for( int i = 0; i < 8; i++ )
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16;
@@ -1966,7 +1972,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         {
             set_func_name( "idct_dequant_2x4_dc_%s", i_cqm?"cqm":"flat" );
             used_asms[1] = 1;
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- )
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             {
                 for( int i = 0; i < 8; i++ )
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16;
@@ -1991,7 +1997,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( #optname ); \
             used_asms[2] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 int qpdc = qp + (size == 8 ? 3 : 0); \
                 int dmf = h->dequant4_mf[CQM_4IC][qpdc%6][0] << qpdc/6; \

jpsdr/x264

common/common.c
@@ -107,8 +107,14 @@ void x264_param_default( x264_param_t *param )
     param->rc.f_vbv_buffer_init = 0.9;
     param->rc.i_qp_constant = -1;
     param->rc.f_rf_constant = 23;
-    param->rc.i_qp_min = 0;
-    param->rc.i_qp_max = INT_MAX;
+    param->rc.i_qp_min_min           =
+    param->rc.i_qp_min[SLICE_TYPE_I] =
+    param->rc.i_qp_min[SLICE_TYPE_P] =
+    param->rc.i_qp_min[SLICE_TYPE_B] = 0;
+    param->rc.i_qp_max_max           =
+    param->rc.i_qp_max[SLICE_TYPE_I] =
+    param->rc.i_qp_max[SLICE_TYPE_P] =
+    param->rc.i_qp_max[SLICE_TYPE_B] = INT_MAX;
     param->rc.i_qp_step = 4;
     param->rc.f_ip_factor = 1.4;
     param->rc.f_pb_factor = 1.3;
@@ -1228,9 +1234,21 @@ int x264_param_parse( x264_param_t *p, const char *name, const char *value )
     OPT("rc-lookahead")
         p->rc.i_lookahead = atoi(value);
     OPT2("qpmin", "qp-min")
-        p->rc.i_qp_min = atoi(value);
+    {
+        if( 3 == sscanf( value, "%d:%d:%d", &p->rc.i_qp_min[SLICE_TYPE_I], &p->rc.i_qp_min[SLICE_TYPE_P], &p->rc.i_qp_min[SLICE_TYPE_B] ) ||
+            3 == sscanf( value, "%d,%d,%d", &p->rc.i_qp_min[SLICE_TYPE_I], &p->rc.i_qp_min[SLICE_TYPE_P], &p->rc.i_qp_min[SLICE_TYPE_B] ) )
+            p->rc.i_qp_min_min = X264_MIN3( p->rc.i_qp_min[SLICE_TYPE_I], p->rc.i_qp_min[SLICE_TYPE_P], p->rc.i_qp_min[SLICE_TYPE_B] );
+        else if( sscanf( value, "%d", &p->rc.i_qp_min_min ) )
+            p->rc.i_qp_min[SLICE_TYPE_I] = p->rc.i_qp_min[SLICE_TYPE_P] = p->rc.i_qp_min[SLICE_TYPE_B] = p->rc.i_qp_min_min;
+    }
     OPT2("qpmax", "qp-max")
-        p->rc.i_qp_max = atoi(value);
+    {
+        if( 3 == sscanf( value, "%d:%d:%d", &p->rc.i_qp_max[SLICE_TYPE_I], &p->rc.i_qp_max[SLICE_TYPE_P], &p->rc.i_qp_max[SLICE_TYPE_B] ) ||
+            3 == sscanf( value, "%d,%d,%d", &p->rc.i_qp_max[SLICE_TYPE_I], &p->rc.i_qp_max[SLICE_TYPE_P], &p->rc.i_qp_max[SLICE_TYPE_B] ) )
+            p->rc.i_qp_max_max = X264_MAX3( p->rc.i_qp_max[SLICE_TYPE_I], p->rc.i_qp_max[SLICE_TYPE_P], p->rc.i_qp_max[SLICE_TYPE_B] );
+        else if( sscanf( value, "%d", &p->rc.i_qp_max_max ) )
+            p->rc.i_qp_max[SLICE_TYPE_I] = p->rc.i_qp_max[SLICE_TYPE_P] = p->rc.i_qp_max[SLICE_TYPE_B] = p->rc.i_qp_max_max;
+    }
     OPT2("qpstep", "qp-step")
         p->rc.i_qp_step = atoi(value);
     OPT("ratetol")
@@ -1864,8 +1882,11 @@ char *x264_param2string( x264_param_t *p, int b_res )
         else
             s += sprintf( s, " bitrate=%d ratetol=%.1f",
                           p->rc.i_bitrate, p->rc.f_rate_tolerance );
-        s += sprintf( s, " qcomp=%.2f qpmin=%d qpmax=%d qpstep=%d",
-                      p->rc.f_qcompress, p->rc.i_qp_min, p->rc.i_qp_max, p->rc.i_qp_step );
+        s += sprintf( s, " qcomp=%.2f qpmin=%d:%d:%d qpmax=%d:%d:%d qpstep=%d",
+                      p->rc.f_qcompress,
+                      p->rc.i_qp_min[SLICE_TYPE_I], p->rc.i_qp_min[SLICE_TYPE_P], p->rc.i_qp_min[SLICE_TYPE_B],
+                      p->rc.i_qp_max[SLICE_TYPE_I], p->rc.i_qp_max[SLICE_TYPE_P], p->rc.i_qp_max[SLICE_TYPE_B],
+                      p->rc.i_qp_step );
         if( p->rc.b_stat_read )
             s += sprintf( s, " cplxblur=%.1f qblur=%.1f",
                           p->rc.f_complexity_blur, p->rc.f_qblur );

encoder/analyse.c
@@ -305,7 +305,7 @@ int x264_analyse_init_costs( x264_t *h )
     for( int i = 1; i <= 2*4*mv_range; i++ )
         logs[i] = log2f( i+1 ) * 2.0f + 1.718f;
 
-    for( int qp = X264_MIN( h->param.rc.i_qp_min, QP_MAX_SPEC ); qp <= h->param.rc.i_qp_max; qp++ )
+    for( int qp = X264_MIN( h->param.rc.i_qp_min_min, QP_MAX_SPEC ); qp <= h->param.rc.i_qp_max_max; qp++ )
         if( init_costs( h, logs, qp ) )
             goto fail;
 
@@ -2947,7 +2947,7 @@ static inline void mb_analyse_qp_rd( x264_t *h, x264_mb_analysis_t *a )
         {
             if( !origcbp )
             {
-                h->mb.i_qp = X264_MAX( h->mb.i_qp - threshold - 1, SPEC_QP( h->param.rc.i_qp_min ) );
+                h->mb.i_qp = X264_MAX( h->mb.i_qp - threshold - 1, SPEC_QP( h->param.rc.i_qp_min[h->sh.i_type] ) );
                 h->mb.i_chroma_qp = h->chroma_qp_table[h->mb.i_qp];
                 already_checked_cost = rd_cost_mb( h, a->i_lambda2 );
                 if( !h->mb.cbp[h->mb.i_mb_xy] )
@@ -2964,7 +2964,7 @@ static inline void mb_analyse_qp_rd( x264_t *h, x264_mb_analysis_t *a )
         }
 
         h->mb.i_qp += direction;
-        while( h->mb.i_qp >= h->param.rc.i_qp_min && h->mb.i_qp <= SPEC_QP( h->param.rc.i_qp_max ) )
+        while( h->mb.i_qp >= h->param.rc.i_qp_min[h->sh.i_type] && h->mb.i_qp <= SPEC_QP( h->param.rc.i_qp_max[h->sh.i_type] ) )
         {
             if( h->mb.i_last_qp == h->mb.i_qp )
                 last_qp_tried = 1;
@@ -3065,7 +3065,7 @@ intra_analysis:
         {
             if( !h->param.analyse.b_psy )
             {
-                mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min ) );
+                mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min[h->sh.i_type] ) );
                 goto intra_analysis;
             }
         }
@@ -3344,8 +3344,7 @@ skip_analysis:
                     h->mc.copy[PIXEL_8x8]  ( h->mb.pic.p_fenc[1], FENC_STRIDE, h->mb.pic.p_fdec[1], FDEC_STRIDE, height );
                     h->mc.copy[PIXEL_8x8]  ( h->mb.pic.p_fenc[2], FENC_STRIDE, h->mb.pic.p_fdec[2], FDEC_STRIDE, height );
                 }
-                mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min ) );
-                goto intra_analysis;
+                mb_analyse_init_qp( h, &analysis, X264_MAX( h->mb.i_qp - h->mb.ip_offset, h->param.rc.i_qp_min[h->sh.i_type] ) );                goto intra_analysis;
             }
 
             if( analysis.i_mbrd >= 2 && h->mb.i_type != I_PCM )

tools/checkasm.c
@@ -2085,8 +2085,14 @@ static int check_quant( int cpu_ref, int cpu_new )
             h->param.i_cqm_preset = h->pps->i_cqm_preset = X264_CQM_CUSTOM;
         }
 
-        h->param.rc.i_qp_min = 0;
-        h->param.rc.i_qp_max = QP_MAX_SPEC;
+        h->param.rc.i_qp_min_min           =
+        h->param.rc.i_qp_min[SLICE_TYPE_I] =
+        h->param.rc.i_qp_min[SLICE_TYPE_P] =
+        h->param.rc.i_qp_min[SLICE_TYPE_B] = 0;
+        h->param.rc.i_qp_max_max           =
+        h->param.rc.i_qp_max[SLICE_TYPE_I] =
+        h->param.rc.i_qp_max[SLICE_TYPE_P] =
+        h->param.rc.i_qp_max[SLICE_TYPE_B] = QP_MAX_SPEC;
         x264_cqm_init( h );
         x264_quant_init( h, 0, &qf_c );
         x264_quant_init( h, cpu_ref, &qf_ref );
@@ -2117,7 +2123,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( #name ); \
             used_asms[0] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int j = 0; j < 2; j++ ) \
                 { \
@@ -2143,7 +2149,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( #qname ); \
             used_asms[0] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int j = 0; j < maxj; j++ ) \
                 { \
@@ -2176,7 +2182,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( "%s_%s", #dqname, i_cqm?"cqm":"flat" ); \
             used_asms[1] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 INIT_QUANT##w(1, w*w) \
                 qf_c.qname( dct1, h->quant##w##_mf[block][qp], h->quant##w##_bias[block][qp] ); \
@@ -2204,7 +2210,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( "%s_%s", #dqname, i_cqm?"cqm":"flat" ); \
             used_asms[1] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int i = 0; i < 16; i++ ) \
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16; \
@@ -2228,8 +2234,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         {
             set_func_name( "idct_dequant_2x4_dc_%s", i_cqm?"cqm":"flat" );
             used_asms[1] = 1;
-            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min]; qp-- )
-            {
+            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min_min]; qp-- )            {
                 for( int i = 0; i < 8; i++ )
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16;
                 qf_c.quant_2x2_dc( &dct1[0], h->quant4_mf[CQM_4IC][qp+3][0]>>1, h->quant4_bias[CQM_4IC][qp+3][0]>>1 );
@@ -2250,8 +2255,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         {
             set_func_name( "idct_dequant_2x4_dconly_%s", i_cqm?"cqm":"flat" );
             used_asms[1] = 1;
-            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min]; qp-- )
-            {
+            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min_min]; qp-- )            {
                 for( int i = 0; i < 8; i++ )
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16;
                 qf_c.quant_2x2_dc( &dct1[0], h->quant4_mf[CQM_4IC][qp+3][0]>>1, h->quant4_bias[CQM_4IC][qp+3][0]>>1 );
@@ -2275,7 +2279,7 @@ static int check_quant( int cpu_ref, int cpu_new )
         { \
             set_func_name( #optname ); \
             used_asms[2] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+            for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 int qpdc = qp + (size == 8 ? 3 : 0); \
                 int dmf = h->dequant4_mf[CQM_4IC][qpdc%6][0] << qpdc/6; \