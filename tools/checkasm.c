14a97dc6d2d2910b1b637af33cda2567dd49e531
jpsdr <jpsdr@wanadoo.fr>
2019-04-08 14:17:47 +0200

    Separate I/P/B qpmin/qpmax parameters.
    Signed-off-by: jpsdr <jpsdr@wanadoo.fr>

--- tools/checkasm.c
+++ tools/checkasm.c
@@ -2159,8 +2159,14 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
             h->param.i_cqm_preset = h->sps->i_cqm_preset = X264_CQM_CUSTOM;
         }
 
-        h->param.rc.i_qp_min = 0;
-        h->param.rc.i_qp_max = QP_MAX_SPEC;
+        h->param.rc.i_qp_min_min           =
+        h->param.rc.i_qp_min[SLICE_TYPE_I] =
+        h->param.rc.i_qp_min[SLICE_TYPE_P] =
+        h->param.rc.i_qp_min[SLICE_TYPE_B] = 0;
+        h->param.rc.i_qp_max_max           =
+        h->param.rc.i_qp_max[SLICE_TYPE_I] =
+        h->param.rc.i_qp_max[SLICE_TYPE_P] =
+        h->param.rc.i_qp_max[SLICE_TYPE_B] = QP_MAX_SPEC;
         x264_cqm_init( h );
         x264_quant_init( h, 0, &qf_c );
         x264_quant_init( h, cpu_ref, &qf_ref );
@@ -2191,7 +2197,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         { \
             set_func_name( #name ); \
             used_asms[0] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+			for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int j = 0; j < 2; j++ ) \
                 { \
@@ -2217,7 +2223,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         { \
             set_func_name( #qname ); \
             used_asms[0] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+			for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int j = 0; j < maxj; j++ ) \
                 { \
@@ -2250,7 +2256,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         { \
             set_func_name( "%s_%s", #dqname, i_cqm?"cqm":"flat" ); \
             used_asms[1] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+			for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 INIT_QUANT##w(1, w*w) \
                 qf_c.qname( dct1, h->quant##w##_mf[block][qp], h->quant##w##_bias[block][qp] ); \
@@ -2278,7 +2284,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         { \
             set_func_name( "%s_%s", #dqname, i_cqm?"cqm":"flat" ); \
             used_asms[1] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+			for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 for( int i = 0; i < 16; i++ ) \
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16; \
@@ -2302,7 +2308,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         {
             set_func_name( "idct_dequant_2x4_dc_%s", i_cqm?"cqm":"flat" );
             used_asms[1] = 1;
-            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min]; qp-- )
+            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min_min]; qp-- )
             {
                 for( int i = 0; i < 8; i++ )
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16;
@@ -2324,7 +2330,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         {
             set_func_name( "idct_dequant_2x4_dconly_%s", i_cqm?"cqm":"flat" );
             used_asms[1] = 1;
-            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min]; qp-- )
+            for( int qp = h->chroma_qp_table[h->param.rc.i_qp_max_max]; qp >= h->chroma_qp_table[h->param.rc.i_qp_min_min]; qp-- )
             {
                 for( int i = 0; i < 8; i++ )
                     dct1[i] = rand()%(PIXEL_MAX*16*2+1) - PIXEL_MAX*16;
@@ -2349,7 +2355,7 @@ static int check_quant( uint32_t cpu_ref, uint32_t cpu_new )
         { \
             set_func_name( #optname ); \
             used_asms[2] = 1; \
-            for( int qp = h->param.rc.i_qp_max; qp >= h->param.rc.i_qp_min; qp-- ) \
+			for( int qp = h->param.rc.i_qp_max_max; qp >= h->param.rc.i_qp_min_min; qp-- ) \
             { \
                 int qpdc = qp + (size == 8 ? 3 : 0); \
                 int dmf = h->dequant4_mf[CQM_4IC][qpdc%6][0] << qpdc/6; \
